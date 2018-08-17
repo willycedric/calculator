ec164f4f4ad0c86328a911bc8035069a9616636b
VOLUNTIS\willy.dongmo <willy.dongmo@voluntis.com>
2018-08-17 16:54:39 +0200

    adding business logic and unit test

---
+++ src/main/java/com/willycedric/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.willycedric.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     int sum(int a, int b) {
+          return a + b;
+     }
+}
